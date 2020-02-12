f16db580f4438df4bd0f4a90f6e4faa5a9fba9e6
Tim <trc22@njit.edu>
2020-02-12 08:04:13 -0500

    Minor annotation

--- game/weapon/WeaponShotgun.cpp
+++ game/weapon/WeaponShotgun.cpp
@@ -164,7 +164,7 @@ stateResult_t rvWeaponShotgun::State_Fire( const stateParms_t& parms ) {
 	switch ( parms.stage ) {
 		case STAGE_INIT:
 			nextAttackTime = gameLocal.time + (fireRate * owner->PowerUpModifier ( PMOD_FIRERATE ));
-			Attack( false, projectiles, spread, 0, 1.0f );
+			Attack( false, projectiles, spread, 0, 1.0f ); //When the shotgun creates projectiles
 			PlayAnim( ANIMCHANNEL_ALL, "fire", 0 );	
 			return SRESULT_STAGE( STAGE_WAIT );
 	
